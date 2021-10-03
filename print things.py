c81483ffa36e62445940f5c6b86d9141f49615f9
conques <70780980+conques@users.noreply.github.com>
2021-10-03 03:27:08 -0300

    Initial commit

---
+++ print things.py
@@ -0,0 +1,7 @@
+print("hello")
+name = input("what is your name")
+print("oh hello " + name + " a pleasure to meet you i am conques")
+
+number = int(input("what is your favorite number"))
+triple = number * 3
+print("your numbre multiplied for 3 equals " + str(triple))
