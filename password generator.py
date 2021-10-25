2fc35ac7e40eeff23cbf6edbf11b588f9a56bb38
conques <70780980+conques@users.noreply.github.com>
2021-10-24 21:05:55 -0300

    guess

--- password generator.py
+++ password generator.py
@@ -2,8 +2,6 @@ import random
 caracteres = """!"#$%&'()*+,-./0123456789:;<=>?ABCDEFGHIJKLMNOPQRSTUVWXYZ[\]^_`abcdefghijklmnopqrstuvwxyz{|}~"""
 n = int(input("numero de claves \n"))
 n2 = int(input("largo de las claves"))
-contador = 0
-contador2 = 0
 
 for clave in range(n):
  claves = ""
